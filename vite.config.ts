026c48c2ae71cdc7c38552c1aef700433efe38d4
Michael Meier <michael-meier@bluesheep.de>
2023-10-01 22:44:02 +0200

    add pwa manifest info and icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,29 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Cookie",
+        short_name: "Cookie",
+        description: "Meal and shopping planner",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   test: { globals: true, environment: "jsdom", setupFiles: "./tests/setup.ts" },
 });
