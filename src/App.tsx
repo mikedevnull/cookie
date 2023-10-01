ea530aac02bdf93d20665b388c505accbd7f644c
Michael Meier <michael-meier@bluesheep.de>
2023-10-01 22:13:54 +0200

    test: add testing and some very basic first test

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "@mantine/core/styles.css";
 import { MantineProvider, Stack, Title } from "@mantine/core";
 import { DEFAULT_THEME } from "@mantine/core";
 import ShopItemList from "./components/ItemList";
+import { Item } from "./components/ListItem";
 import "./App.css";
 import SearchAndAddItemField from "./components/SearchAndAddItemField";
-const items = [
-  { name: "Foo" },
-  { name: "Bar" },
-  { name: "Baz" },
-  { name: "Fuu" },
+const items: Item[] = [
+  { name: "Foo", marked: true },
+  { name: "Bar", marked: true },
+  { name: "Baz", marked: false },
+  { name: "Fuu", marked: true },
 ];
 
 function App() {
