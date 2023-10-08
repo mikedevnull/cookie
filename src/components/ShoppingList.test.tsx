7e9a2ab852f3dfd59c785a1bc0803f3ac3a4ba34
Michael Meier <michael-meier@bluesheep.de>
2023-10-08 14:18:18 +0200

    feat: mark clicked items as inactive

---
+++ src/components/ShoppingList.test.tsx
@@ -0,0 +1,72 @@
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
+import ShoppingList from "./ShoppingList";
+import userEvent from "@testing-library/user-event";
+import { Database } from "@/db";
+import { DbTestContext } from "@testing/setup";
+
+async function setupTestData(db: Database) {
+  await db.collections.items.bulkInsert([
+    {
+      name: "testItem1",
+      active: false,
+    },
+    {
+      name: "testItem2",
+      active: true,
+    },
+    {
+      name: "testItem3",
+      active: true,
+    },
+  ]);
+}
+
+test<DbTestContext>("Render items in database", async ({ db, render }) => {
+  await setupTestData(db);
+
+  render(<ShoppingList />);
+
+  await screen.findAllByRole("listitem");
+
+  const items = await screen.getAllByRole("listitem");
+  expect(items.length).toBe(2);
+  expect(items.at(0)).toHaveTextContent("testItem2");
+  expect(items.at(1)).toHaveTextContent("testItem3");
+});
+
+test<DbTestContext>("Clicking an item toggles state in database", async ({
+  db,
+  render,
+}) => {
+  await setupTestData(db);
+
+  render(<ShoppingList />);
+
+  await screen.findByText("testItem2");
+
+  const item = screen.getByText("testItem2");
+  assert(item !== undefined);
+  const removalFinished = waitForElementToBeRemoved(
+    screen.getByText("testItem2")
+  );
+  await userEvent.click(item);
+  await removalFinished;
+});
+
+test<DbTestContext>("Removal from database removes from ", async ({
+  db,
+  render,
+}) => {
+  await setupTestData(db);
+
+  render(<ShoppingList />);
+
+  await screen.findByText("testItem2");
+
+  const removalFinished = waitForElementToBeRemoved(
+    screen.getByText("testItem2")
+  );
+
+  await db.collections.items.findOne("testItem2").remove();
+  await removalFinished;
+});
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,5 +1,6 @@
-import { ItemCollection } from "@/db";
+import { Item, ItemCollection } from "@/db";
 import ItemList from "./ItemList";
+import { RxDocument } from "rxdb";
 import { useRxData } from "rxdb-hooks";
 
 export default function ShoppingList() {
@@ -11,10 +12,19 @@ export default function ShoppingList() {
     })
   );
 
+  const itemSelectedCallback = (item: Item) => {
+    const document = items.find(
+      (doc: RxDocument<Item>) => doc.name === item.name
+    );
+    if (document) {
+      document.patch({ active: !item.active });
+    }
+  };
+
   return (
     <section>
       <h2>Shopping list</h2>
-      <ItemList items={items}></ItemList>
+      <ItemList items={items} itemClicked={itemSelectedCallback}></ItemList>
     </section>
   );
 }
