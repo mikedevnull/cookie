c9b2e53bc90c8736a537158fec3b33ff65ffd665
Michael Meier <michael-meier@bluesheep.de>
2023-10-08 09:32:39 +0200

    add database provider and wrapper for tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Database, initialize } from "./db";
-import { Provider } from "rxdb-hooks";
+import App from "@/App.tsx";
+import "@/index.css";
+import { DatabaseProvider } from "@/db";
 
-export const Root = () => {
-  const [db, setDb] = useState<Database>();
-
-  useEffect(() => {
-    initialize().then(setDb);
-  }, []);
-
-  return (
-    <Provider db={db}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<Root />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <DatabaseProvider>
+      <App />
+    </DatabaseProvider>
+  </React.StrictMode>
+);
--- tests/render.tsx
+++ tests/render.tsx
@@ -1,12 +1,15 @@
-import { render as testingLibraryRender } from "@testing-library/react";
+import {
+  RenderOptions,
+  render as testingLibraryRender,
+} from "@testing-library/react";
 import React from "react";
 import { MantineProvider } from "@mantine/core";
 import { DEFAULT_THEME } from "@mantine/core";
 
-export function render(ui: React.ReactNode) {
-  return testingLibraryRender(<>{ui}</>, {
-    wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={DEFAULT_THEME}>{children}</MantineProvider>
-    ),
-  });
+export function render(ui: React.ReactElement, options?: RenderOptions) {
+  const AllProviders = ({ children }: { children: React.ReactNode }) => {
+    return <MantineProvider theme={DEFAULT_THEME}>{children}</MantineProvider>;
+  };
+
+  return testingLibraryRender(ui, { wrapper: AllProviders, ...options });
 }
