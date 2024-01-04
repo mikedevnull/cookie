08826500a2e990905b5353f0b261e5a8e5693d67
Michael Meier <michael-meier@bluesheep.de>
2024-01-04 21:19:38 +0100

    feat: add simple pwa auto-update confirmation

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
 import { DatabaseProvider } from "@/db";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <DatabaseProvider>
