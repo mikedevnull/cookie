47e33f9e9f2d8c3a292644c6c7854b258155a340
Michael Meier <michael-meier@bluesheep.de>
2024-01-05 09:35:01 +0100

    feat: show message when list is empty

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -3,6 +3,7 @@ import { Item, ItemCollection } from "@/db";
 import { RxDocument } from "rxdb";
 import { useRxData } from "rxdb-hooks";
 import ShopItemList from "./ShopItemList";
+import { Fade, Typography } from "@mui/material";
 
 export default function ShoppingList() {
   const { result: items } = useRxData("items", (collection: ItemCollection) =>
@@ -22,12 +23,28 @@ export default function ShoppingList() {
     }
   };
 
-  return (
-    <section>
+  if (items.length === 0) {
+    return (
+      <Fade in={items.length === 0}>
+        <Typography
+          variant="body2"
+          color="primary.light"
+          fontWeight={"bold"}
+          marginTop={4}
+          padding={8}
+          textAlign={"center"}
+          textTransform={"uppercase"}
+        >
+          The list is currently empty.
+        </Typography>
+      </Fade>
+    );
+  } else {
+    return (
       <ShopItemList
         items={items}
         itemSelectedCallback={itemSelectedCallback}
       ></ShopItemList>
-    </section>
-  );
+    );
+  }
 }
