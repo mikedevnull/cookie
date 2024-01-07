cfe4d99a6a6e1aca12ffa45a90c6dea62191e3f1
Michael Meier <michael-meier@bluesheep.de>
2024-01-07 10:42:59 +0100

    feat: add debounced callback for existing item search to add textfield

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,4 @@
-import { afterEach, vi } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import { Database, createDatabase } from "@/db";
@@ -7,6 +7,7 @@ import { createRenderWithDb } from "./render";
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+  vi.resetAllMocks();
 });
 
 export interface DbTestContext {
@@ -21,18 +22,4 @@ beforeEach<DbTestContext>(async (context) => {
 
 afterEach<DbTestContext>(async (context) => {
   await context.db.remove();
-});
-
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: vi.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(), // deprecated
-    removeListener: vi.fn(), // deprecated
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  })),
 });
