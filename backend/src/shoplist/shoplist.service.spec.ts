e332a724f816598b14a326df0ea5d48ea959e01d
Michael Meier <michael-meier@bluesheep.de>
2024-01-28 19:41:15 +0100

    feat(backend): add naive implementation for push endpoint

--- backend/src/shoplist/shoplist.service.spec.ts
+++ backend/src/shoplist/shoplist.service.spec.ts
@@ -1,23 +1,188 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import ShoplistService from './shoplist.service';
 import { ShopListItemEntity } from './shoplistitem.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ShopListItem } from './shoplistitem.interface';
+
+const itemA400 = {
+  name: 'Item A',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 400,
+} as const;
+const itemB200 = {
+  name: 'Item B',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 200,
+} as const;
+const itemC500 = {
+  name: 'Item C',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 500,
+} as const;
+const itemD900 = {
+  name: 'Item D',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 900,
+} as const;
+const itemE300 = {
+  name: 'Item E',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 300,
+} as const;
+const itemF300 = {
+  name: 'Item F',
+  rank: 90,
+  active: true,
+  _deleted: false,
+  lastUpdate: 300,
+} as const;
+
+const testData: ShopListItem[] = [
+  itemC500,
+  itemA400,
+  itemF300,
+  itemB200,
+  itemE300,
+  itemD900,
+] as const;
 
 describe('ShoplistService', () => {
   let service: ShoplistService;
 
+  let repository: Repository<ShopListItemEntity>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        ShoplistService,
-        { provide: getRepositoryToken(ShopListItemEntity), useValue: 'FIXME' },
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [ShopListItemEntity],
+          synchronize: true,
+          dropSchema: true,
+        }),
+        TypeOrmModule.forFeature([ShopListItemEntity]),
       ],
+      providers: [ShoplistService],
     }).compile();
 
     service = module.get<ShoplistService>(ShoplistService);
+    repository = module.get(getRepositoryToken(ShopListItemEntity));
+    await repository
+      .createQueryBuilder()
+      .insert()
+      .into(ShopListItemEntity)
+      .values(testData)
+      .execute();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return items after last checkpoint in order', async () => {
+    const result = await service.findAllAfter(200, 'Item B', 5);
+
+    expect(result).toEqual([itemE300, itemF300, itemA400, itemC500, itemD900]);
+  });
+
+  it('should take into account name ordering when change times are equal', async () => {
+    const result = await service.findAllAfter(300, 'Item E', 5);
+
+    expect(result).toEqual([itemF300, itemA400, itemC500, itemD900]);
+  });
+
+  it('should adhere to requested item limit', async () => {
+    const result = await service.findAllAfter(0, '', 3);
+
+    expect(result).toEqual([itemB200, itemE300, itemF300]);
+  });
+
+  it('should insert new items into the database', async () => {
+    const conflicts = await service.createOrUpdate([
+      {
+        newDocumentState: {
+          name: 'Foo',
+          rank: 42,
+          active: true,
+          _deleted: false,
+          lastUpdate: 1000,
+        },
+      },
+    ]);
+
+    expect(conflicts.length).toBe(0);
+    const itemInDb = await repository.findOneBy({ name: 'Foo' });
+    expect(itemInDb).toEqual({
+      name: 'Foo',
+      rank: 42,
+      active: true,
+      _deleted: false,
+      lastUpdate: 1000,
+    });
+  });
+
+  it('should update existing items in the database', async () => {
+    const newItem = {
+      ...itemA400,
+      active: !itemA400.active,
+      lastUpdate: 1000,
+    } as const;
+    const conflicts = await service.createOrUpdate([
+      { newDocumentState: newItem, assumedMasterState: itemA400 },
+    ]);
+
+    expect(conflicts.length).toBe(0);
+
+    const itemInDb = await repository.findOneBy({ name: 'Item A' });
+    expect(itemInDb).toEqual(newItem);
+  });
+
+  it('should detect and report conflicts if assumed state mismatches', async () => {
+    const newItem = {
+      ...itemA400,
+      active: !itemA400.active,
+      lastUpdate: 1000,
+    } as const;
+    const conflicts = await service.createOrUpdate([
+      {
+        newDocumentState: newItem,
+        assumedMasterState: { ...itemA400, lastUpdate: 300 },
+      },
+    ]);
+
+    expect(conflicts).toEqual([itemA400]);
+
+    const itemInDb = await repository.findOneBy({ name: 'Item A' });
+    expect(itemInDb).toEqual(itemA400);
+  });
+
+  it('should detect and report conflicts if no assumed state but item exists', async () => {
+    const newItem = {
+      ...itemA400,
+      active: !itemA400.active,
+      lastUpdate: 1000,
+    } as const;
+    const conflicts = await service.createOrUpdate([
+      {
+        newDocumentState: newItem,
+      },
+    ]);
+
+    expect(conflicts).toEqual([itemA400]);
+
+    const itemInDb = await repository.findOneBy({ name: 'Item A' });
+    expect(itemInDb).toEqual(itemA400);
   });
 });
--- backend/src/shoplist/shoplist.service.ts
+++ backend/src/shoplist/shoplist.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { ShopListItemEntity } from './shoplistitem.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MoreThan, Repository } from 'typeorm';
+import { In, MoreThan, Repository } from 'typeorm';
 import { ShopListItem } from './shoplistitem.interface';
 import { DocumentChangeRow } from 'src/replication';
 
+export interface ShopListServiceInterface {
+  findAllAfter: (
+    updatedAt: number,
+    id: string,
+    limit?: number,
+  ) => Promise<ShopListItem[]>;
+
+  createOrUpdate: (
+    changedRows: DocumentChangeRow<ShopListItem>[],
+  ) => Promise<ShopListItem[]>;
+}
+
 @Injectable()
-export default class ShoplistService {
+export default class ShoplistService implements ShopListServiceInterface {
   constructor(
     @InjectRepository(ShopListItemEntity)
     private _itemRepository: Repository<ShopListItemEntity>,
@@ -27,7 +39,50 @@ export default class ShoplistService {
     });
   }
 
-  async createOrUpdate(updateDocuments: DocumentChangeRow<ShopListItem>[]) {
-    return updateDocuments;
+  async createOrUpdate(
+    changedRows: DocumentChangeRow<ShopListItem>[],
+  ): Promise<ShopListItem[]> {
+    // neither optimal nor pretty, but should get the job done in a first attempt
+    const conflicts = await this._itemRepository.manager.transaction(
+      async () => {
+        const ids = changedRows.map((r) => r.newDocumentState.name);
+        const existingDocuments = await this._itemRepository.find({
+          select: { name: true, lastUpdate: true },
+          where: { name: In(ids) },
+        });
+        const conflicts: ShopListItemEntity[] = [];
+        for (const row of changedRows) {
+          const itemInDb = existingDocuments.find(
+            (d) => d.name === row.newDocumentState.name,
+          );
+          if (itemInDb) {
+            // check for conflict
+            if (
+              !row.assumedMasterState ||
+              (row.assumedMasterState &&
+                itemInDb.lastUpdate !== row.assumedMasterState.lastUpdate)
+            ) {
+              conflicts.push(itemInDb);
+            } else {
+              this._itemRepository.update(
+                row.newDocumentState.name,
+                row.newDocumentState,
+              );
+            }
+          } else {
+            // must be a new item
+            this._itemRepository.insert(row.newDocumentState);
+          }
+          return conflicts;
+        }
+      },
+    );
+
+    if (conflicts.length > 0) {
+      return this._itemRepository.find({
+        where: { name: In(conflicts.map((c) => c.name)) },
+      });
+    }
+    return [];
   }
 }
