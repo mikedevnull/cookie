057132634b882c4fe5e28759e7cb9e517f345394
Michael Meier <michael-meier@bluesheep.de>
2024-02-02 22:14:20 +0100

    feat(frontend): Selecting item from suggested list adds it to main list
    Fixes #64

--- frontend/src/components/AddItemTextField.test.tsx
+++ frontend/src/components/AddItemTextField.test.tsx
@@ -1,6 +1,7 @@
 import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent, { UserEvent } from "@testing-library/user-event";
 import AddItemTextField from "./AddItemTextField";
+import { useState } from "react";
 
 let user: UserEvent;
 beforeEach(() => {
@@ -16,7 +17,22 @@ afterEach(() => {
 test("Enter text and press enter submits and resets", async () => {
   const submit = jest.fn();
 
-  render(<AddItemTextField submitValue={submit} />);
+  type Props = {
+    submitValue?: (arg: string) => void;
+  };
+
+  const WrappedAddItemTextField = ({ submitValue }: Props) => {
+    const [value, setValue] = useState<string>("");
+    return (
+      <AddItemTextField
+        value={value}
+        onChange={setValue}
+        submitValue={submitValue}
+      />
+    );
+  };
+
+  render(<WrappedAddItemTextField submitValue={submit} />);
 
   const input =
     await screen.findByPlaceholderText<HTMLInputElement>("Add item");
@@ -31,8 +47,10 @@ test("Enter text and press enter submits and resets", async () => {
   expect(submit).toHaveBeenCalledWith("Foobar");
 });
 
-test("Does use default value as text", async () => {
-  render(<AddItemTextField defaultValue="MyNiceValue" />);
+test("Does use value as text", async () => {
+  const value = "MyNiceValue";
+  const setValue = jest.fn();
+  render(<AddItemTextField value={value} onChange={setValue} />);
 
   const input =
     await screen.findByPlaceholderText<HTMLInputElement>("Add item");
@@ -42,8 +60,12 @@ test("Does use default value as text", async () => {
 
 test("Does not submit on empty texts", async () => {
   const submit = jest.fn();
+  const value = "";
+  const setValue = jest.fn();
 
-  render(<AddItemTextField submitValue={submit} />);
+  render(
+    <AddItemTextField value={value} onChange={setValue} submitValue={submit} />
+  );
   const input =
     await screen.findByPlaceholderText<HTMLInputElement>("Add item");
   expect(input).toBeInTheDocument();
@@ -52,48 +74,16 @@ test("Does not submit on empty texts", async () => {
   expect(submit).not.toHaveBeenCalled();
 });
 
-test("Updates search input with more than three character entered", async () => {
-  const mockSearchCallback = jest.fn();
-
-  render(<AddItemTextField searchFilterCallback={mockSearchCallback} />);
-  const input = await screen.findByPlaceholderText("Add item");
-  await user.type(input, "123");
-
-  act(() => jest.advanceTimersByTime(100));
-  expect(mockSearchCallback).not.toHaveBeenCalled();
-});
-
-test("Updates search input with more than three character entered", async () => {
-  const mockSearchCallback = jest.fn();
-
-  render(<AddItemTextField searchFilterCallback={mockSearchCallback} />);
-
-  const input = await screen.findByPlaceholderText("Add item");
-  await user.type(input, "123");
-
-  await user.type(input, "45");
-  act(() => jest.advanceTimersByTime(50));
-  await user.type(input, "67");
-
-  act(() => jest.runOnlyPendingTimers());
-  expect(mockSearchCallback).toHaveBeenCalledTimes(1);
-  expect(mockSearchCallback).toHaveBeenCalledWith("1234567");
-});
-
 test("Clicking the clear icon removes all input text", async () => {
-  const mockSearchCallback = jest.fn();
+  const value = "123";
+  const setValue = jest.fn();
 
-  render(<AddItemTextField searchFilterCallback={mockSearchCallback} />);
+  render(<AddItemTextField value={value} onChange={setValue} />);
 
-  const input =
-    await screen.findByPlaceholderText<HTMLInputElement>("Add item");
   const clearButton = await screen.findByTestId("input-clear-button");
-  await user.type(input, "123");
 
   await user.click(clearButton);
-  act(() => jest.advanceTimersByTime(400));
 
-  expect(input.value).toBe("");
-  expect(mockSearchCallback).toHaveBeenCalled();
-  expect(mockSearchCallback).toHaveBeenLastCalledWith("");
+  expect(setValue).toHaveBeenCalled();
+  expect(setValue).toHaveBeenLastCalledWith("");
 });
