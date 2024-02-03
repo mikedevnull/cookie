bb45f55fdec93849a8d6d685715aa6a4b7f8124e
Michael Meier <michael-meier@bluesheep.de>
2024-02-03 19:17:15 +0100

    feat(backend): add logging middleware to log api access

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShoplistModule } from './shoplist/shoplist.module';
 import { ShopListItemEntity } from './shoplist/shoplistitem.entity';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -22,4 +23,8 @@ import { ShopListItemEntity } from './shoplist/shoplistitem.entity';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/');
+  }
+}
