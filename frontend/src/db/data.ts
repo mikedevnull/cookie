7e42b27ea61694f42fa68a4dc4635305fd539465
Michael Meier <michael-meier@bluesheep.de>
2024-04-11 23:34:37 +0200

    feat(frontend): more "realistic" initial dummy data

--- frontend/src/db/data.ts
+++ frontend/src/db/data.ts
@@ -1,11 +1,31 @@
 import type { Item } from "./types";
 
-function createItems() {
-  const items = new Array<Item>();
-  for (let i = 0; i < 30; ++i) {
-    items.push({ name: `Item ${i}`, active: i % 2 === 0, rankOrder: 0 });
-  }
-  return items;
-}
-
-export const initialItems: Item[] = createItems();
+export const initialItems: Item[] = [
+  { name: "Äpfel", active: true },
+  { name: "Bananen", active: false },
+  { name: "Tomaten", active: true },
+  { name: "Brot", active: false },
+  { name: "Butter", active: true },
+  { name: "Backpulver", active: false },
+  { name: "Duschgel", active: true },
+  { name: "Hackfleisch", active: false },
+  { name: "Schnitzel", active: true },
+  { name: "Käse", active: false },
+  { name: "Ketchup", active: true },
+  { name: "Zahnpasta", active: false },
+  { name: "Milch", active: true },
+  { name: "Sojamilch", active: false },
+  { name: "Nudeln", active: true },
+  { name: "Pommes Frites", active: false },
+  { name: "Reis", active: true },
+  { name: "Salz", active: false },
+  { name: "Tortellini", active: true },
+  { name: "Waschmittel", active: false },
+  { name: "Zitrone", active: true },
+].map(
+  (n) =>
+    ({
+      ...n,
+      rankOrder: 0,
+    }) satisfies Item
+);
