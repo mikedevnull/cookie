01cac86fec045a9dc0f232af2004f1e3f2168506
Michael Meier <michael-meier@bluesheep.de>
2024-04-26 20:49:23 +0200

    feat(frontend): add page to add items to list

--- frontend/src/pages/AddItem/AddItem.tsx
+++ frontend/src/pages/AddItem/AddItem.tsx
@@ -1,22 +1,58 @@
 import { IconButton, Button, Container, TextField } from "@mui/material";
 
 import BackIcon from "@mui/icons-material/ArrowBack";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useSubmit } from "react-router-dom";
 import { AppNavBar } from "@/components/AppNavBar";
+import SuggestedItemList from "@/components/SuggestedItemList";
+import debounce from "lodash.debounce";
+import { ChangeEventHandler, useEffect, useMemo, useState } from "react";
+import { useRxData } from "rxdb-hooks";
+import { Item, ItemCollection } from "@/db";
+
+const BackAction = (
+  <IconButton
+    edge="start"
+    color="inherit"
+    component={Link}
+    to="/"
+    aria-label="back"
+  >
+    <BackIcon />
+  </IconButton>
+);
+
+function useSuggestedItems(searchText: string): Item[] {
+  const cleanText = searchText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&").trim();
+  const itemsQuerys = useRxData("items", (collection: ItemCollection) => {
+    return collection?.find({
+      selector: {
+        name: { $regex: cleanText, $options: "i" },
+      },
+    });
+  });
+
+  return itemsQuerys.result;
+}
 
 export default function AddItem() {
-  const BackAction = (
-    <IconButton
-      edge="start"
-      color="inherit"
-      component={Link}
-      to="/"
-      aria-label="back"
-    >
-      <BackIcon />
-    </IconButton>
-  );
+  const [searchText, setSearchText] = useState<string>("");
 
+  const suggestedItems = useSuggestedItems(searchText);
+
+  const debounceSearch = useMemo(
+    () => debounce(setSearchText, 500, { leading: true }),
+    []
+  );
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = function (event) {
+    debounceSearch(event.currentTarget.value);
+  };
+  useEffect(() => {
+    return () => {
+      debounceSearch.cancel();
+    };
+  }, [debounceSearch]);
+
+  const submit = useSubmit();
   return (
     <>
       <Form method="post">
@@ -27,6 +63,7 @@ export default function AddItem() {
         </AppNavBar>
         <Container maxWidth="sm" sx={{ marginTop: 4 }}>
           <TextField
+            onChange={onInputChange}
             fullWidth
             name="itemName"
             autoFocus
@@ -34,6 +71,16 @@ export default function AddItem() {
           />
         </Container>
       </Form>
+      <Container maxWidth="sm" sx={{ marginTop: 4 }}>
+        <SuggestedItemList
+          items={suggestedItems}
+          itemSelectedCallback={(item) => {
+            const formData = new FormData();
+            formData.append("itemName", item.name);
+            submit(formData, { method: "post" });
+          }}
+        />
+      </Container>
     </>
   );
 }
