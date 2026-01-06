0759fc459f1fb4ca0b18cbbd695704934e75a1c8
Michael Meier <michael-meier@bluesheep.de>
2026-01-06 12:14:37 +0100

    feat(frontend): add first draft of checkable list item

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,37 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
+import CheckableItem from "./components/checkable-item.tsx";
+import { useState } from "react";
+
+type Item = {
+  name: string;
+  checked: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [item, setItem] = useState<Item>({ name: "Foo", checked: false });
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="cookie logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>cookie</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CheckableItem
+        checked={item.checked}
+        label={item.name}
+        changeCallback={(data) =>
+          setItem({ name: data.label, checked: data.checked })
+        }
+      />
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ packages/frontend/src/components/checkable-item.spec.tsx
@@ -0,0 +1,66 @@
+import { expect, test, vi } from "vitest";
+import { render } from "vitest-browser-react";
+import CheckableItem from "./checkable-item";
+
+test("renders the unchecked checkbox correctly", async () => {
+  const label = "Test Label";
+  const cb = vi.fn();
+  const { getByRole } = await render(
+    <CheckableItem label={label} changeCallback={cb} checked={false} />
+  );
+
+  const checkbox = getByRole("checkbox", { name: label });
+  expect(checkbox).toBeInTheDocument();
+  expect(checkbox).not.toBeChecked();
+
+  await checkbox.click();
+  expect(cb).toHaveBeenCalledExactlyOnceWith({ checked: true, label: label });
+});
+
+test("renders the checked checkbox correctly", async () => {
+  const label = "Test Label";
+  const cb = vi.fn();
+  const { getByRole } = await render(
+    <CheckableItem label={label} changeCallback={cb} checked={true} />
+  );
+
+  const checkbox = getByRole("checkbox", { name: label });
+  expect(checkbox).toBeInTheDocument();
+  expect(checkbox).toBeChecked();
+
+  await checkbox.click();
+  expect(cb).toHaveBeenCalledExactlyOnceWith({ checked: false, label: label });
+});
+
+test("changing label calls callback with new label", async () => {
+  const label = "Test Label";
+  const cb = vi.fn();
+  const { getByRole } = await render(
+    <CheckableItem label={label} changeCallback={cb} checked={false} />
+  );
+
+  const labelEdit = getByRole("textbox");
+  expect(labelEdit).toBeInTheDocument();
+  expect(labelEdit).toHaveValue(label);
+
+  await labelEdit.fill("A new label");
+  expect(cb).toHaveBeenCalledExactlyOnceWith({
+    checked: false,
+    label: "A new label",
+  });
+});
+
+test("Editing label without changing actual value does not call callback", async () => {
+  const label = "Test Label";
+  const cb = vi.fn();
+  const { getByRole } = await render(
+    <CheckableItem label={label} changeCallback={cb} checked={true} />
+  );
+
+  const labelEdit = getByRole("textbox");
+  expect(labelEdit).toBeInTheDocument();
+  expect(labelEdit).toHaveValue(label);
+
+  await labelEdit.fill(label);
+  expect(cb).not.toHaveBeenCalled();
+});
