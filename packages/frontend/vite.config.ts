6f3d5d790e49e162c38f478c84dfff2f6d711843
Michael Meier <michael-meier@bluesheep.de>
2026-01-06 12:14:13 +0100

    test(frontend): add vitest browser testing

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,11 +2,20 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { playwright } from '@vitest/browser-playwright'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     globals: true,
+     browser: {
+      enabled: true,
+      provider: playwright(),
+      // https://vitest.dev/config/browser/playwright
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
   },
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
