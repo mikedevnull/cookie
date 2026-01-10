36a78083484490e8c553379ebe975023ef5cdbfb
Michael Meier <michael-meier@bluesheep.de>
2026-01-10 10:51:20 +0100

    feat(frontend): first simple editable list with changeable items implementation

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import PWABadge from "./PWABadge.tsx";
 import "./App.css";
-import CheckableItem from "./components/checkable-item.tsx";
-import { useState } from "react";
+
 import { DatabaseProvider } from "./db/provider.tsx";
 import { clearDatabase } from "./db/database.ts";
-
-type Item = {
-  name?: string;
-  checked: boolean;
-};
+import ShopList from "./pages/ShopList.tsx";
 
 function App() {
-  const [item, setItem] = useState<Item>({ name: "Foo", checked: false });
-
   let resetDatabaseButton;
   if (import.meta.env.DEV) {
     resetDatabaseButton = (
@@ -30,13 +23,7 @@ function App() {
   return (
     <>
       <DatabaseProvider>
-        <CheckableItem
-          checked={item.checked}
-          label={item.name}
-          changeCallback={(data) =>
-            setItem({ name: data.label, checked: data.checked })
-          }
-        />
+        <ShopList />
       </DatabaseProvider>
       {resetDatabaseButton}
       <PWABadge />
