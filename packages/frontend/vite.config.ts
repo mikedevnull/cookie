705159fb72acba883e7762fbd961319168fd92ef
Michael Meier <michael-meier@bluesheep.de>
2026-01-24 13:50:51 +0000

    chore(frontend): formatting

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest/config" />
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { playwright } from '@vitest/browser-playwright'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,55 +12,53 @@ export default defineConfig({
       enabled: true,
       provider: playwright(),
       // https://vitest.dev/config/browser/playwright
-      instances: [
-        { browser: 'chromium' },
-      ],
+      instances: [{ browser: "chromium" }],
     },
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if(id.includes('rxdb') || id.includes('rxjs')) {
-              return 'rxdb'
+          if (id.includes("node_modules")) {
+            if (id.includes("rxdb") || id.includes("rxjs")) {
+              return "rxdb";
             }
-            if(id.includes('react')) {
-              return 'react'
+            if (id.includes("react")) {
+              return "react";
             }
-            return 'vendor'; // Split vendor libraries
+            return "vendor"; // Split vendor libraries
           }
-        }
+        },
       },
     },
   },
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      manifest: {
+        name: "cookie",
+        short_name: "cookie",
+        description: "cookie",
+        theme_color: "#ffffff",
+      },
 
-    manifest: {
-      name: 'cookie',
-      short_name: 'cookie',
-      description: 'cookie',
-      theme_color: '#ffffff',
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
-
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
