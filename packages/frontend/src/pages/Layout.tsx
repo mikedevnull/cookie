a098f1bc749ac5ac905b36db063a8efe5743e12a
Michael Meier <michael-meier@bluesheep.de>
2026-02-01 17:42:50 +0100

    feat(frontend): use daisyui for pwa infos
    also, better integration in page layout

--- packages/frontend/src/pages/Layout.tsx
+++ packages/frontend/src/pages/Layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import { NavBar } from "../components/navbar";
+import PWABadge from "../PWABadge";
 
 interface Props extends React.PropsWithChildren {
     pageMenuItems?: React.JSX.Element
@@ -8,6 +9,7 @@ interface Props extends React.PropsWithChildren {
 export function MainLayout({ children, pageMenuItems }: Props) {
     return <div className="flex flex-col h-full justify-between">
         <NavBar menuItems={pageMenuItems} />
+        <PWABadge></PWABadge>
         <main className="p-4 grow self-center w-full md:w-3xl">
             {children}
         </main>
