1ee8bf3c855d95712bf519e15058499665c7fec1
Michael Meier <michael-meier@bluesheep.de>
2026-02-11 19:55:22 +0100

    fix(frontend): make react router public base path into account

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router";
 
+const basePath = import.meta.env.BASE_URL
+console.log(basePath)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basePath}>
       <App />
     </BrowserRouter>
   </StrictMode>,
